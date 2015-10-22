0098dcc9dce30c47fbfee611970afd9321eccdde
Ling <wssgcg1213@qq.com>
2015-10-22 15:44:11 +0800

    json

---
+++ getJSON.php
@@ -0,0 +1,23 @@
+<?php
+header("Content-Type: application/json");
+
+$data = [
+    "nav" => "你爷爷",
+    "data" => [[
+        "title" => "ling",
+        "url" => "https://zeroling.com/"
+    ],[
+        "title" => "baidu",
+        "url" => "https://baidu.com/"
+    ],[
+        "title" => "ling blog",
+        "url" => "https://www.zeroling.com/"
+    ],[
+        "title" => "redrock",
+        "url" => "http://hongyan.cqupt.edu.cn/"
+    ],[
+        "title" => "BridgeTest😈",
+        "url" => "http://api.zeroling.com/LingJSBridgeTest/"
+    ]]
+
+];
